6575223c8c522067e440e71e8b76fbe68ed7b556
Issac Roy <issacroy05@gmail.com>
2025-04-05 13:07:31 -0700

    created pose displayer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
-import PoseCamera from "./components/PoseCamera"
-import Scene from "./components/Scene"
+import PoseCamera from "./components/PoseCamera";
+import Scene from "./components/Scene";
 
 function App() {
-
   return (
     <div className="h-screen flex flex">
-      <Scene />
+      <div className="w-1/2">
+        <Scene />
+      </div>
       <PoseCamera />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
